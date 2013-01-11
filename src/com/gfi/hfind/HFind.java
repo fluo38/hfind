bce3c5183618de48f756408a23a2ab8e7a820f99
Ludovic B. <ludovic.bertsch@gfi.fr>
2013-01-11 14:53:33 +0100

    Not exactly it...

--- src/com/gfi/hfind/HFind.java
+++ src/com/gfi/hfind/HFind.java
@@ -9,9 +9,17 @@ public class HFind {
 	private boolean follow_links; 
 	
 	public void hfind(LinkedList<File> directory_list, int max_depth) {
+		File current;
 		int length = directory_list.size();
 		for (int i = 0; i < length; i++) {
 			// Something like that!
+			current = directory_list.poll();
+			File[] children = current.listFiles();
+			for (int j = 0; j < children.length; j++) {
+				if (children[j].isDirectory()) {
+					directory_list.addLast(children[j]);
+				}
+			}
 		}
 	}
 	
@@ -28,5 +36,5 @@ public class HFind {
 		
 		System.exit(0);
 	}
-
 }
+
