273ce42619a401ca030be2bf7756e058cd5c4f4f
Ludovic B <ludovic.bertsch@gfi.fr>
2013-01-11 15:20:31 +0100

    Min depth implemented and seems to work!

--- src/com/gfi/hfind/HFind.java
+++ src/com/gfi/hfind/HFind.java
@@ -9,20 +9,22 @@ public class HFind {
 	//private boolean follow_links;
 	private boolean display_files       = false;
 	private boolean display_directories = true;
-	private int     min_depth = 0; // Not implemented yet!
+	private int     min_depth = 1; // Not implemented yet!
+	private int     max_depth = 2;
 	
-	public void hfind(LinkedList<File> directory_list, int max_depth) {
+	public void hfind(LinkedList<File> directory_list) {
 		File current;
+		int depth = 0;
 		
-		while (directory_list.size() != 0 && max_depth >= 0) {
+		while (directory_list.size() != 0 && depth <= max_depth) {
 			int length = directory_list.size();
 			for (int i = 0; i < length; i++) {
 				// Something like that!
 				current = directory_list.poll();
-				if (display_directories) {
+				if (display_directories && depth >= min_depth) {
 					System.out.println(current);
 				}
-				if (max_depth >= 1) {
+				if (depth < max_depth) {
 					File[] children = current.listFiles();
 					if (children != null) {
 						for (int j = 0; j < children.length; j++) {
@@ -35,7 +37,7 @@ public class HFind {
 					}
 				}
 			}
-			max_depth--;
+			depth++;
 		}
 	}
 	
@@ -48,7 +50,7 @@ public class HFind {
 		
 		list.add(new File("/"));
 		
-		object.hfind(list, 2);
+		object.hfind(list);
 		
 		System.exit(0);
 	}
