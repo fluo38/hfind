4eb5904e8bddbc10badcd272f837555b35a0b133
Ludovic B <ludovic.bertsch@gfi.fr>
2013-01-31 12:17:36 +0100

    OKDOK

--- src/com/gfi/hfind/HFind.java
+++ src/com/gfi/hfind/HFind.java
@@ -8,13 +8,8 @@ public class HFind {
 	// Options
 	private boolean display_files       = false;
 	private boolean display_directories = true;
-<<<<<<< HEAD
-	private int     min_depth = 1; // Included!
-	private int     max_depth = 2; // Included!
-=======
 	private int     min_depth = 1; // Value is included!
 	private int     max_depth = 2; // Value is included!
->>>>>>> origin/boulot
 	
 	public void setDisplayFiles(boolean display) {
 		display_files = display;
@@ -50,9 +45,6 @@ public class HFind {
 	 * Another message
 	 */
 	public static void displayUsage() {
-<<<<<<< HEAD
-		System.err.println("Usage: hfind [--min-depth x] [--max-depth y] <directory>");
-=======
 		System.err.println("Usage: hfind [--min-depth x] [--max-depth y] <directories...>");
 		System.err.println("Other options: ");
 		System.err.println("  --display-files");
@@ -61,21 +53,12 @@ public class HFind {
 		System.err.println("  --do-not-display-directories");
 		System.err.println("  --level l");
 		System.err.println("  --help");
->>>>>>> origin/boulot
 	}
 	
 	public void hfind(LinkedList<File> directory_list) {
 		File current;
 		int depth = 0;
 		
-<<<<<<< HEAD
-		while (directory_list.size() != 0 && depth <= max_depth) {
-			int length = directory_list.size();
-			for (int i = 0; i < length; i++) {
-				// Something like that!
-				current = directory_list.poll();
-				if (display_directories && depth >= min_depth) {
-=======
 		// Warn if finding regular files in the directory list...
 		for (int i = 0; i < directory_list.size(); i++) {
 			if (!directory_list.get(i).isDirectory()) {
@@ -91,7 +74,6 @@ public class HFind {
 			for (int i = 0; i < length; i++) {
 				current = directory_list.poll();
 				if (display_directories && current.isDirectory() && depth >= min_depth) {
->>>>>>> origin/boulot
 					System.out.println(current);
 				}
 				if (depth < max_depth) {
@@ -128,14 +110,6 @@ public class HFind {
 				} else if (args[i].equals("--min-depth")) {
 					object.setMinDepth(Integer.getInteger(args[i + 1]));
 					i++;
-<<<<<<< HEAD
-				} else if (args[i].equals("--display-files")) {
-					object.setDisplayFiles(true);
-				} else if (args[i].equals("--display-directories")) {
-					object.setDisplayDirectories(true);
-				} else {
-					System.err.println("Unknown argument \"" + args[i] + "\"...");
-=======
 				} else if (args[i].equals("--level")) {
 					assert(Integer.getInteger(args[i + 1]) >= 0); // We need to be careful!
 					object.setMinDepth(Integer.getInteger(args[i + 1]));
@@ -155,36 +129,19 @@ public class HFind {
 				} else {
 					System.err.println("Unknown argument \"" + args[i] + "\"...");
 					displayUsage();
->>>>>>> origin/boulot
 					System.exit(1);
 				}
 				i++;
 			}
-<<<<<<< HEAD
 			list.add(new File(args[i]));
 		} else {
 			//list.add(new File("."));
 			list.add(new File("/"));
 		}
 		
-		object.hfind("/");
-		//blabla
-=======
-			
-			// Assuming all remaining arguments are directories to go through...
-			while (i < args.length) {
-				list.add(new File(args[i]));
-				i++;
-			}
-		} else {
-			// Assuming no argument means from the current directory
-			list.add(new File("."));
-		}
-		
 		//object.hfind(list);
 		object.hfind("/");
 		
->>>>>>> origin/boulot
 		System.exit(0);
 	}
 }
